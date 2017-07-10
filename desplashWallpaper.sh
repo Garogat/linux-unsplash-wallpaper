a3e7f1f596095d30704882390015740e591352b3
Garogat <anton@ju60.de>
2017-07-10 19:59:03 +0200

    Update desplashWallpaper.sh

--- desplashWallpaper.sh
+++ desplashWallpaper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Images are taken from the "hell so easy to use (basic) API" of Unsplash, they are totally free for all use.
+PATH=/usr/local/bin:${PATH}
 
 # The downloaded images are kept in ~/.local/share/unsplashLinux if the following switch is false.
 DO_WE_ERASE_FILES=true
@@ -11,7 +12,7 @@ DESKTOP="XFCE"
 SIZE="1920x1080"
 
 # Set home directory so cronjob can run this script
-#HOME=""
+HOME="/home/anton"
 
 WORKDIR=$HOME'/.local/share/unsplashLinux/'
 RANT=$(date +%s)
@@ -34,6 +35,13 @@ wget -O ${WORKDIR}${RANT}'.jpg' -q https://source.unsplash.com/$SIZE
 if [ "$DESKTOP" == "GNOME" ]; then
         gsettings set org.gnome.desktop.background picture-uri 'file://'${WORKDIR}${RANT}'.jpg'
 elif [ "$DESKTOP" == "XFCE" ]; then
+        # Cron support
+        pid=$(ps -C xfce4-session -o pid=)
+        # Hack to remove the leading space. Maybe not so nice, but it works.
+        pid=$(echo $pid)
+        # Get the environment variable from /proc
+        export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ)
+
         for i in $(xfconf-query -c xfce4-desktop -p /backdrop -l|egrep -e "screen.*/monitor.*image-path$" -e "screen.*/monitor.*/last-image$"); do
             xfconf-query -c xfce4-desktop -p $i -n -t string -s ${WORKDIR}${RANT}'.jpg'
             xfconf-query -c xfce4-desktop -p $i -s ${WORKDIR}${RANT}'.jpg'
